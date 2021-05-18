502c1d49575350d588e338ef67923ecb9d821a3d
mrgian <mail@gian.im>
2021-05-18 11:47:10 +0200

    Add home bottom bar navigation

--- app/src/main/java/im/gian/tr/home/HomeActivity.kt
+++ app/src/main/java/im/gian/tr/home/HomeActivity.kt
@@ -2,14 +2,21 @@ package im.gian.tr.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import im.gian.tr.R
-import im.gian.tr.databinding.ActivityLoginBinding
+import im.gian.tr.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this,R.layout.activity_home)
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_bar_menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, findNavController(R.id.navHostFragment))
     }
 }
---
+++ app/src/main/java/im/gian/tr/home/fragments/MapFragment.kt
@@ -0,0 +1,36 @@
+package im.gian.tr.home.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import im.gian.tr.R
+import im.gian.tr.databinding.FragmentHomeBinding
+import im.gian.tr.databinding.FragmentMapBinding
+import im.gian.tr.databinding.FragmentSigninBinding
+import im.gian.tr.home.HomeViewModel
+import im.gian.tr.intro.IntroViewModel
+
+class MapFragment : Fragment() {
+    val homeViewModel: HomeViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentMapBinding>(
+            inflater, R.layout.fragment_map, container, false)
+
+        binding.map = this
+        binding.homeViewModel = homeViewModel
+
+
+
+
+        return binding.root
+    }
+}
