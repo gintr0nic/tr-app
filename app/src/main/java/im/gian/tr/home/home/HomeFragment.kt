aae5a833d5ef17a8b6a424395891920ad8515666
mrgian <mail@gian.im>
2021-06-10 18:34:26 +0200

    Add restaurant search

--- app/src/main/java/im/gian/tr/home/home/HomeFragment.kt
+++ app/src/main/java/im/gian/tr/home/home/HomeFragment.kt
@@ -1,20 +1,22 @@
 package im.gian.tr.home.home
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import im.gian.tr.R
 import im.gian.tr.databinding.FragmentHomeBinding
 import im.gian.tr.home.HomeViewModel
+import im.gian.tr.model.Restaurant
+
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -30,6 +32,15 @@ class HomeFragment : Fragment() {
         binding.home = this
         binding.lifecycleOwner = this
         binding.homeViewModel = homeViewModel
+
+        //Search textview
+        binding.textViewSearch.setAdapter(SuggestionAdapter(context as FragmentActivity, R.layout.suggestion_view, homeViewModel.restaurants.value!!))
+        binding.textViewSearch.threshold = 1
+        val restaurantsObserver = Observer<List<Restaurant>> {
+            binding.textViewSearch.setAdapter(SuggestionAdapter(context as FragmentActivity, R.layout.suggestion_view, homeViewModel.restaurants.value!!))
+        }
+        homeViewModel.restaurants.observe(context as FragmentActivity, restaurantsObserver)
+
 
         //Restaurants recyclerview
         val adapter = RestaurantCardAdapter(context, false)
---
+++ app/src/main/java/im/gian/tr/home/home/SuggestionAdapter.kt
@@ -0,0 +1,73 @@
+package im.gian.tr.home.home
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.TextView
+import com.google.gson.Gson
+import im.gian.tr.R
+import im.gian.tr.home.HomeActivity
+import im.gian.tr.model.Restaurant
+import im.gian.tr.restaurant.RestaurantActivity
+
+
+class SuggestionAdapter(context: Context, private val layoutResource: Int, private val restaurants: List<Restaurant>):
+    ArrayAdapter<Restaurant>(context, layoutResource, restaurants),
+    Filterable {
+    private var restaurantList: List<Restaurant> = restaurants
+
+    override fun getCount(): Int {
+        return restaurantList.size
+    }
+
+    override fun getItem(position: Int): Restaurant? {
+        return restaurantList[position]
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(layoutResource, parent, false)
+
+        view.setOnClickListener {
+            val intent = Intent(context, RestaurantActivity::class.java)
+            intent.putExtra("restaurant", Gson().toJson(restaurantList[position]))
+
+            //if (restaurantList[position] in savedList!!)
+            //    intent.putExtra("saved", true)
+
+            context.startActivity(intent)
+        }
+
+        val name = view.findViewById<TextView>(R.id.suggestionName)
+        name.text = restaurantList[position].name
+
+        return view
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults) {
+                restaurantList = filterResults.values as List<Restaurant>
+                notifyDataSetChanged()
+            }
+
+            override fun performFiltering(charSequence: CharSequence?): FilterResults {
+                val queryString = charSequence?.toString()?.lowercase()
+
+                val filterResults = FilterResults()
+                filterResults.values = if (queryString == null || queryString.isEmpty())
+                    restaurants
+                else
+                    restaurants.filter {
+                        it.name.lowercase().contains(queryString)
+                    }
+                return filterResults
+            }
+        }
+    }
+}
